b8d181370b8b748e07d80548a1ae7884946c94c1
marc <marc@symbioticsoftware.com>
2019-03-02 11:14:46 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import { createModel, createCollection } from './lib/createResource';
+import useCollection from './lib/useCollection';
+import useModel from './lib/useModel';
+import useObserver from './lib/useObserver';
+import useSuspense from './lib/useSuspense';
+
+export default {
+  createCollection,
+  createModel,
+  useCollection,
+  useModel,
+  useObserver,
+  useSuspense,
+};
